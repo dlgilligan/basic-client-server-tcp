e7eb6c46cd94cf6e80aae61f0cf265404c1ae633
Daniel Gilligan <86537777+dlgilligan@users.noreply.github.com>
2022-07-02 18:18:42 -0400

    Add files via upload

---
+++ server/TCP_sender.h
@@ -0,0 +1,14 @@
+
+#ifndef C_1_TCP_SENDER_H
+#define C_1_TCP_SENDER_H
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif //C_1_TCP_SENDER_H
